07aabb0309911d60fe5d5ff1526ed33d5b652730
devdoris <devdoris1@gmail.com>
2026-01-10 23:40:54 +0100

    Second Assignment.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage for CSE231? ");
+        string response = Console.ReadLine();
+        int number = int.Parse(response);
+
+        string grade = "";
+
+        if (number >= 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade for CSE231 is: {grade}");
+        
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed CSE231");
+        }
+        else
+        {
+            Console.WriteLine("Uh oh, try harder next time!");
+        }
     }
 }
