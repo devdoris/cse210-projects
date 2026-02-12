1a20d885b5da201aa4109f8e90cc08205c9103db
devdoris <devdoris1@gmail.com>
2026-02-12 18:24:00 +0100

    Learning Activity.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of Shape objects
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different types of shapes
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Iterate through the list
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
